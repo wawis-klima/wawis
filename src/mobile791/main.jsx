a5a18bc930f23aaa17c8e4bc95403483af25f5ab
wawis-klima <wasik@wawis.pl>
2026-09-11 14:56:23 +0200

    WAWIS 10.62: podłącz styl kontrahentów

--- src/mobile791/main.jsx
+++ src/mobile791/main.jsx
@@ -4,4 +4,5 @@ import App from './App.jsx'
 import './styles.css'
 import './styles/desktop-jobs-table.css'
 import './v1056-new-job-spacing.css'
+import './v1062-contractors-mobile.css'
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
