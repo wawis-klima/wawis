6da548c22829119b6b511cb953ab9136c84f928d
wawis-klima <wasik@wawis.pl>
2026-09-11 14:59:45 +0200

    WAWIS 10.63: wczytaj styl Kontrahenci 10.62 w runtime mobile

--- src/main.jsx
+++ src/main.jsx
@@ -53,6 +53,7 @@ async function boot() {
     await import('./mobile791/v1048-runtime-fix.css')
     await import('./mobile791/v1055-new-job-polish.css')
     await import('./mobile791/v1057-new-job-form-fix.css')
+    await import('./mobile791/v1062-contractors-mobile.css')
   }
 
   diagnosticsModule.installDiagnosticConsoleCapture()
