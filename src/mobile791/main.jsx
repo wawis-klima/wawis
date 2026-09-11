3ded76c1c0b95b4cbf306341ec048ac81c9cbd5d
wawis-klima <wasik@wawis.pl>
2026-09-11 15:04:36 +0200

    WAWIS 10.64: dołącz kompaktowe style kontrahentów

--- src/mobile791/main.jsx
+++ src/mobile791/main.jsx
@@ -5,4 +5,5 @@ import './styles.css'
 import './styles/desktop-jobs-table.css'
 import './v1056-new-job-spacing.css'
 import './v1062-contractors-mobile.css'
+import './v1064-contractors-compact.css'
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
