81237ebdeaae950e7ea37f2b51e73fdefde12b29
wawis-klima <wasik@wawis.pl>
2026-09-11 08:58:28 +0200

    WAWIS 10.56: podłącz poprawki formularza nowego zlecenia

--- src/mobile791/main.jsx
+++ src/mobile791/main.jsx
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './styles/desktop-jobs-table.css'
+import './v1056-new-job-spacing.css'
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
